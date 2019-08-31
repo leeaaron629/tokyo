9270d5b2d51e3d8463df8037e35bb1ef09954273
Aaron Lee <leeaaron326@gmail.com>
2019-08-30 23:56:42 -0400

    Updating dependency

--- mix.exs
+++ mix.exs
@@ -24,8 +24,8 @@ defmodule Tokyo.MixProject do
     [
       {:plug_cowboy, "~> 2.1.0"},
       {:jason, "~> 1.1"},
-      {:ecto_sql, "~> 3.0"},
-      {:postgrex, "~> 0.0.0"},
+      {:ecto_sql, "~> 3.1.6"},
+      {:postgrex, "~> 0.15.0"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
