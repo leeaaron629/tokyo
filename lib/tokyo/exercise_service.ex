5b0defa9d10e1ecd8cfa1472a920061698634cd0
Aaron Lee <leeaaron326@gmail.com>
2019-09-04 08:28:27 -0400

    Removed unnecessary code

--- lib/tokyo/exercise_service.ex
+++ lib/tokyo/exercise_service.ex
@@ -21,25 +21,4 @@ defmodule Tokyo.ExerciseService do
         IO.puts "Deleting exercise with id = #{id}"
     end
 
-    defp payload_to_struct(empty_struct, payload) do
-        
-        string_keys_map = 
-            Map.from_struct(empty_struct)
-            |> Map.to_list
-            |> Enum.map(fn {k, v} -> {Atom.to_string(k), v} end)
-            |> Map.new
-            |> Map.delete("__meta__")
-
-        payload_with_atom_keys = 
-            for {k, v} <- payload, into: %{} do
-                case Map.fetch(string_keys_map, k) do
-                    {:ok, _val} -> {String.to_existing_atom(k), v}
-                    :error -> {k, v}
-                end
-            end
-        
-        struct(empty_struct.__struct__, payload_with_atom_keys)
-
-    end
-
 end
