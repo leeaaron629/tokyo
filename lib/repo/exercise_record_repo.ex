12587a14283153e6c987156ad429b3f86bddd255
Aaron Lee <leeaaron326@gmail.com>
2019-12-05 12:29:14 -0500

    Adding Get an Exercise Record. + Formatting

--- lib/repo/exercise_record_repo.ex
+++ lib/repo/exercise_record_repo.ex
@@ -1,57 +1,53 @@
 defmodule Tokyo.Repo.ExerciseRecord do
-    
-    use GenServer
+  use GenServer
 
-    @me __MODULE__
+  @me __MODULE__
 
-    def init(initial_state) do
-        {:ok, initial_state}
-    end
+  def init(initial_state) do
+    {:ok, initial_state}
+  end
 
-    def start_link(initial_state) do
-        GenServer.start_link(@me, initial_state, name: @me)
-    end
+  def start_link(initial_state) do
+    GenServer.start_link(@me, initial_state, name: @me)
+  end
 
-    # Get Exercise Records from the user
+  # Get Exercise Records from the user
 
-    def get_exercise_records(user_id) do
-        GenServer.call(@me, {:get_ex_rec, user_id})
-    end
+  def get_exercise_records(user_id) do
+    GenServer.call(@me, {:get_ex_rec, user_id})
+  end
 
-    # Save Exercise Records from the user
+  # Save Exercise Records from the user
 
-    def save_exercise_records(ex_rec, user_id) do
-        GenServer.cast(@me, {:save_ex_rec, ex_rec, user_id})
-    end
+  def save_exercise_records(ex_rec, user_id) do
+    GenServer.cast(@me, {:save_ex_rec, ex_rec, user_id})
+  end
 
-    # Remove Exercise Record from the user
+  # Remove Exercise Record from the user
 
-    def remove_exercise_records(ex_rec_id, user_id) do
-        GenServer.cast(@me, {:remove_ex_rec, ex_rec_id, user_id})
-    end
+  def remove_exercise_records(ex_rec_id, user_id) do
+    GenServer.cast(@me, {:remove_ex_rec, ex_rec_id, user_id})
+  end
 
-    def handle_call({:get_ex_rec, user_id}, _from, current_state) do
-        # IO.puts "Current state: #{inspect current_state}"
-        # IO.puts "Return value: #{inspect current_state[user_id]}"
-        {:reply, current_state[user_id], current_state}
-    end
+  def handle_call({:get_ex_rec, user_id}, _from, current_state) do
+    # IO.puts "Current state: #{inspect current_state}"
+    # IO.puts "Return value: #{inspect current_state[user_id]}"
+    {:reply, current_state[user_id], current_state}
+  end
 
-    def handle_cast({:remove_ex_rec, ex_rec_id, user_id}, current_state) do
-        
-        exercise_records = Map.get(current_state, user_id)
-        updated_exercise_records = Map.delete(exercise_records, ex_rec_id)
+  def handle_cast({:remove_ex_rec, ex_rec_id, user_id}, current_state) do
+    exercise_records = Map.get(current_state, user_id)
+    updated_exercise_records = Map.delete(exercise_records, ex_rec_id)
 
-        new_state = Map.put(current_state, user_id, updated_exercise_records)
-        {:noreply, new_state}
-    end
+    new_state = Map.put(current_state, user_id, updated_exercise_records)
+    {:noreply, new_state}
+  end
 
-    def handle_cast({:save_ex_rec, ex_rec, user_id}, current_state) do
-        
-        exercise_records = Map.get(current_state, user_id, %{})
-        updated_exercise_records = Map.put(exercise_records, ex_rec.ex_rec_id, ex_rec)
+  def handle_cast({:save_ex_rec, ex_rec, user_id}, current_state) do
+    exercise_records = Map.get(current_state, user_id, %{})
+    updated_exercise_records = Map.put(exercise_records, ex_rec.ex_rec_id, ex_rec)
 
-        new_state = Map.put(current_state, user_id, updated_exercise_records)
-        {:noreply, new_state} 
-    end
-
+    new_state = Map.put(current_state, user_id, updated_exercise_records)
+    {:noreply, new_state}
+  end
 end
--- lib/services/exercise_record_service.ex
+++ lib/services/exercise_record_service.ex
@@ -1,36 +1,42 @@
 defmodule Tokyo.Service.ExerciseRecord do
+  alias Tokyo.Repo.ExerciseRecord
 
-    alias Tokyo.Repo.ExerciseRecord
+  def fetch_exercise_records_by_user_id(user_id) do
+    IO.puts("Fetching exercises for #{user_id}")
 
-    def fetch_exercise_records_by_user_id(user_id) do
-        IO.puts "Fetching exercises for #{user_id}"
-        case exercise_records = ExerciseRecord.get_exercise_records(user_id) do
-            nil -> []
-            _ -> Map.values(exercise_records)
-        end
+    case exercise_records = ExerciseRecord.get_exercise_records(user_id) do
+      nil -> []
+      _ -> Map.values(exercise_records)
     end
+  end
 
-    def fetch_an_exercise_record(user_id, ex_rec_id) do
-        IO.puts "Fetching exercise record for #{user_id} [#{ex_rec_id}]"
+  def fetch_an_exercise_record(user_id, ex_rec_id) do
+    IO.puts("Fetching exercise record for #{user_id} [#{ex_rec_id}]")
 
+    case exercise_records = ExerciseRecord.get_exercise_records(user_id) do
+      nil -> %{}
+      _ -> exercise_records
     end
+    |> Map.get(ex_rec_id, nil)
+  end
 
-    def save_exercise_rec(ex_rec, user_id) do
-        IO.puts "Saving exercise record for #{inspect user_id}"
-        IO.inspect ex_rec
+  def save_exercise_rec(ex_rec, user_id) do
+    IO.puts("Saving exercise record for #{inspect(user_id)}")
+    IO.inspect(ex_rec)
 
-        ex_rec_to_save = case Map.get(ex_rec, :ex_rec_id) do
-            nil -> Map.put(ex_rec, :ex_rec_id, Ecto.UUID.generate)
-            _-> ex_rec
-        end
-        ExerciseRecord.save_exercise_records(ex_rec_to_save, user_id)
+    ex_rec_to_save =
+      case Map.get(ex_rec, :ex_rec_id) do
+        nil -> Map.put(ex_rec, :ex_rec_id, Ecto.UUID.generate())
+        _ -> ex_rec
+      end
 
-        ex_rec_to_save
-    end
+    ExerciseRecord.save_exercise_records(ex_rec_to_save, user_id)
 
-    def delete_exercise_rec(id, user_id) do
-        IO.puts "Deleting exercise record #{inspect id} from #{inspect user_id}"
-        ExerciseRecord.remove_exercise_records(id, user_id)
-    end
+    ex_rec_to_save
+  end
 
+  def delete_exercise_rec(id, user_id) do
+    IO.puts("Deleting exercise record #{inspect(id)} from #{inspect(user_id)}")
+    ExerciseRecord.remove_exercise_records(id, user_id)
+  end
 end
