db6dd59bbe03e947aefb2e34fb5078ae8a2bea52
Aaron Lee <leeaaron326@gmail.com>
2020-01-23 11:41:56 -0500

    Updated config to use environment variables

--- config/config.exs
+++ config/config.exs
@@ -4,9 +4,9 @@ import Config
 config :tokyo, ecto_repos: [Tokyo.Repo]
 
 config :tokyo, Tokyo.Repo,
-  database: "tokyo_repo",
-  username: "postgres",
-  password: "abc123",
+  database: "tokyo",
+  username: System.fetch_env!("PSQL_USERNAME"),
+  password: System.fetch_env!("PSQL_PASSWORD"),
   hostname: "localhost"
 
 config :tokyo, initial_repo_state: %{}
