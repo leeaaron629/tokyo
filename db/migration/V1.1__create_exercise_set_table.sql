035127422c98abfdd2aa215ba98662179a9682d3
Aaron Lee <leeaaron326@gmail.com>
2020-05-25 14:16:18 -0400

    Updated tables to be plural

--- db/migration/V1.1__create_exercise_set_table.sql
+++ db/migration/V1.1__create_exercise_set_table.sql
@@ -1,9 +1,9 @@
 -- Create Exercise Set Table
 
-create table exercise_set 
+create table exercise_sets
   ( 
      ex_set_id uuid not null, 
-     ex_rec_id uuid references exercise_record(ex_rec_id), 
+     ex_rec_id uuid references exercise_records(ex_rec_id), 
      reps      integer not null default 0, 
      sets      integer not null default 0, 
      primary key(ex_set_id) 
