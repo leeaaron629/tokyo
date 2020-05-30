0adf3eef8345a1f3e2cb961545abb28f26a57761
Aaron Lee <leeaaron326@gmail.com>
2020-05-30 19:20:54 -0400

    Added exercise_set schema

--- db/migration/V1.1__create_exercise_set_table.sql
+++ db/migration/V1.1__create_exercise_set_table.sql
@@ -2,9 +2,9 @@
 
 create table exercise_sets
   ( 
-     ex_set_id uuid not null, 
-     ex_rec_id uuid references exercise_records(ex_rec_id), 
-     rep      integer not null default 0, 
-     "set"      integer not null default 0, 
+     ex_set_id uuid  not null, 
+     ex_rec_id uuid  references exercise_records(ex_rec_id), 
+     weight          integer not null default 0, 
+     reps            integer not null default 0, 
      primary key(ex_set_id) 
   );
