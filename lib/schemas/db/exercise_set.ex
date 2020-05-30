3a13c8a18056c8e828ba0219eae625172d49a9f3
Aaron Lee <leeaaron326@gmail.com>
2020-05-30 19:20:54 -0400

    Added exercise_set schema

---
+++ lib/schemas/db/exercise_set.ex
@@ -0,0 +1,34 @@
+defmodule Tokyo.Db.ExerciseSet do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @schema_prefix "tokyo"
+  @primary_key {:ex_set_id, :binary_id, []}
+
+  @fields [
+    :ex_set_id,
+    :ex_rec_id,
+    :weight,
+    :reps
+  ]
+
+  @required_fields [
+    :ex_rec_id,
+    :weight,
+    :reps
+  ]
+
+  schema "exercise_sets" do
+    field :ex_set_id, :binary_id,
+    field :ex_rec_id, :binary_id,
+    field :weight, :integer,
+    field :reps, :integer 
+  end
+
+  def changeset(exercise_set, params \\ %{}) do 
+    exercise_set
+      |> cast(params, @fields)
+      |> validate_required(@required_fields)
+  end
+
+end
